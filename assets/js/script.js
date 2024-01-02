0e7462c288f7fffc05e31ed37f8d324191e00bf4
Haruto Nguyen <ngxvanquan@gmail.com>
2024-01-02 21:51:40 +0700

    build basic structure dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,102 @@
+console.log("%cHello🙌", "font-size: 20px; color:green");
+
+console.log(
+    "%c👉 This Website is build with html, css(sass), javascript and a lots of love",
+    "font-size: 15px; color:green"
+);
+
+console.log(
+    "%c👉 This is the last project in the f8 course html css pro😎",
+    "font-size: 15px; color:green"
+);
+
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+/**
+ * Hàm tải template
+ *
+ * Cách dùng:
+ * <div id="parent"></div>
+ * <script>
+ *  load("#parent", "./path-to-template.html");
+ * </script>
+ */
 function loadHTML(selector, path) {
     const cached = localStorage.getItem(path);
     if (cached) {
-        document.querySelector(selector).innerHTML = cached;
+        $(selector).innerHTML = cached;
     }
 
     fetch(path)
         .then((res) => res.text())
         .then((html) => {
             if (html !== cached) {
-                document.querySelector(selector).innerHTML = html;
+                $(selector).innerHTML = html;
                 localStorage.setItem(path, html);
             }
+        })
+        .finally(() => {
+            window.dispatchEvent(new Event("template-loaded"));
         });
 }
+
+/**
+ * Hàm kiểm tra một phần tử
+ * có bị ẩn bởi display: none không
+ */
+function isHidden(element) {
+    if (!element) return true;
+
+    if (window.getComputedStyle(element).display === "none") {
+        return true;
+    }
+
+    let parent = element.parentElement;
+    while (parent) {
+        if (window.getComputedStyle(parent).display === "none") {
+            return true;
+        }
+        parent = parent.parentElement;
+    }
+
+    return false;
+}
+
+/**
+ * Hàm buộc một hành động phải đợi
+ * sau một khoảng thời gian mới được thực thi
+ */
+function debounce(func, timeout = 300) {
+    let timer;
+    return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            func.apply(this, args);
+        }, timeout);
+    };
+}
+
+/**
+ * Hàm tính toán vị trí arrow cho dropdown
+ *
+ * Cách dùng:
+ * 1. Thêm class "js-dropdown-list" vào thẻ ul cấp 1
+ * 2. CSS "left" cho arrow qua biến "--arrow-left-pos"
+ */
+const calArrowPos = debounce(() => {
+    if (isHidden($(".js-dropdown-list"))) return;
+
+    const items = $$(".js-dropdown-list > li");
+
+    items.forEach((item) => {
+        const arrowPos = item.offsetLeft + item.offsetWidth / 2;
+        item.style.setProperty("--arrow-left-pos", `${arrowPos}px`);
+    });
+});
+
+// Tính toán lại vị trí arrow khi resize trình duyệt
+window.addEventListener("resize", calArrowPos);
+
+// Tính toán lại vị trí arrow sau khi tải template
+window.addEventListener("template-loaded", calArrowPos);
